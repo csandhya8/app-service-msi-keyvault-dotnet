8f42d273edb489bd4d22cb54e6ad43a12a60dee1
csandhya8 <sandhyasekarnct@gmail.com>
2019-07-17 12:09:18 +0530

    Update HomeController.cs

--- WebAppKeyVault/WebAppKeyVault/Controllers/HomeController.cs
+++ WebAppKeyVault/WebAppKeyVault/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace WebAppKeyVault.Controllers
                 var keyVaultClient = new KeyVaultClient(
                     new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
 
-                var secret = await keyVaultClient.GetSecretAsync("https://appmsi5kv.vault.azure.net/secrets/secret")
+                var secret = await keyVaultClient.GetSecretAsync("https://keyvault-dotnet.vault.azure.net/secrets/TheSecret")
                     .ConfigureAwait(false);
 
                 ViewBag.Secret = $"Secret: {secret.Value}";
